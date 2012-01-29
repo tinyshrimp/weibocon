2770a24f8197f17e43ca23191030dff36a189c3c
tinyshrimp <tinyshrimp@workstation.(none)>
2012-01-29 23:36:34 +0800

    create weibocon project.

---
+++ src/weibocon/org/WeiboconActivity.java
@@ -0,0 +1,13 @@
+package weibocon.org;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeiboconActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
