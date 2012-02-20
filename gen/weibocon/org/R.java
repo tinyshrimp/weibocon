ae708e63e6c458e1f96cbb6b0075f13142f366be
U-SPSS\xiwang <xiwang@IBM-L3ACP3R.spss.com>
2012-02-20 21:53:54 +0800

    merge pull requests from leonluo.

--- gen/weibocon/org/R.java
+++ gen/weibocon/org/R.java
@@ -11,13 +11,55 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int go_previous_5=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int logo_b=0x7f020003;
+        public static final int title_back_normal=0x7f020004;
+        public static final int triangle=0x7f020005;
+        public static final int welcome_h=0x7f020006;
+        public static final int welcome_v=0x7f020007;
+    }
+    public static final class id {
+        public static final int button1=0x7f050008;
+        public static final int editText1=0x7f05000c;
+        public static final int editText2=0x7f05000d;
+        public static final int imageView1=0x7f050002;
+        public static final int imageView2=0x7f050004;
+        public static final int listView1=0x7f050007;
+        public static final int relativeLayout1=0x7f050000;
+        public static final int relativeLayout2=0x7f050003;
+        public static final int spinner1=0x7f05000e;
+        public static final int textView1=0x7f050001;
+        public static final int textView2=0x7f050005;
+        public static final int textView3=0x7f050006;
+        public static final int textView4=0x7f05000b;
+        public static final int textView5=0x7f050009;
+        public static final int textView6=0x7f05000a;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int info_detail_display=0x7f030000;
+        public static final int info_fans=0x7f030001;
+        public static final int info_friends=0x7f030002;
+        public static final int info_personal_display=0x7f030003;
+        public static final int info_personal_modify=0x7f030004;
+        public static final int info_user_detail=0x7f030005;
+        public static final int search_main=0x7f030006;
+        public static final int sys_about=0x7f030007;
+        public static final int sys_account=0x7f030008;
+        public static final int sys_login=0x7f030009;
+        public static final int sys_setting=0x7f03000a;
+        public static final int sys_welcome=0x7f03000b;
+        public static final int weibo_detail=0x7f03000c;
+        public static final int weibo_forward=0x7f03000d;
+        public static final int weibo_home=0x7f03000e;
+        public static final int weibo_reply_display=0x7f03000f;
+        public static final int weibo_reply_edit=0x7f030010;
+        public static final int weibo_write=0x7f030011;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int sys_setting=0x7f040002;
     }
 }
